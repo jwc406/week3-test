e58074740a6638d54e287e4840bb788f4f7e0acd
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-07-13 10:28:54 +0900

    완료

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -1,30 +1,43 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 /**
  * 2초 지연을 시키는 함수입니다 (비동기 작업).
  */
-import { waitTwoSeconds } from '../../utils';
+import { waitTwoSeconds } from "../../utils";
 
 export const __addToDo = createAsyncThunk(
-  '__addToDo',
-  async (payload, thunkAPI) => {}
+  "__addToDo",
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(addTodo(payload));
+  }
 );
 
 export const __deleteTodo = createAsyncThunk(
-  '__deleteToDo',
-  async (payload, thunkAPI) => {}
+  "__deleteToDo",
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(deleteTodo(payload));
+  }
 );
 
 const initialState = {
-  list: [],
+  list: [{ id: 1, title: "리액트 공부하기", body: "리액트 심화 강의 듣기" }],
 };
 
 const todosSlice = createSlice({
-  name: 'todos',
+  name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action) => {},
-    deleteTodo: (state, action) => {},
+    addTodo: (state, action) => {
+      state.list.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      const index = state.list.findIndex((todo) => todo.id === action.payload);
+      if (index !== -1) {
+        state.list.splice(index, 1);
+      }
+    },
   },
 });
 
